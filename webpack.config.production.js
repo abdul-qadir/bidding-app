04b5295328225bfe9c9571d314240bb2bab9f827
abdul-qadir <abdul.mansoori@appirio.com>
2017-09-03 18:43:09 +0530

    set env

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const autoprefixer = require('autoprefixer');
+const config = require('config');
 
 module.exports = {
   devtool: 'source-map',
@@ -25,7 +26,12 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
-      __DEV__: JSON.stringify(process.env.NODE_ENV),
+      __DEV__: JSON.stringify(config.NODE_ENV),
+      AUTH0_CALLBACK_URL: JSON.stringify(config.AUTH0_CALLBACK_URL),
+      AUTH0_CLIENT_SECRET: JSON.stringify(config.AUTH0_CLIENT_SECRET),
+      AUTH0_CLIENT_ID: JSON.stringify(config.AUTH0_CLIENT_ID),
+      AUTH0_DOMAIN: JSON.stringify(config.AUTH0_DOMAIN),
+      AUTH_DOMAIN: JSON.stringify(config.AUTH_DOMAIN),
     }),
     new ExtractTextPlugin('css/[name]-[hash].min.css'),
   ],
